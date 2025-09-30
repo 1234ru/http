2e931d835f1bdaf836edd0ba3c4cb8e7ecef5cae
1234ru <1234ru@gmail.com>
2025-09-30 17:26:17 +0300

    printResponse() made public

--- Request.php
+++ Request.php
@@ -326,7 +326,7 @@ class Request
         return $output;
     }
 
-    private function printResponse()
+    public function printResponse()
     {
         $msg = '';
         foreach ($this->response['headers'] as $key => $value) {
